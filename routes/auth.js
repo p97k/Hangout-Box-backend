71a1f17e2e9fde0188b51a46a837d079e56778fb
Parham <p97kh76@gmail.com>
2022-07-10 13:43:16 +0430

    feat: push repo to git

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,7 @@ router.post('/login', async (req, res) => {
     } catch (error) {
         res.status(500).send({
             error,
-            detail: 'internal server error'
+            detail: 'internal server error!'
         })
     }
 })
