6b04444c64885300e6ab68d494132ea2afb5f64f
Parham <p97kh76@gmail.com>
2022-07-10 20:01:06 +0430

    fix: remove unused assignment

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ router.put("/:id", async (req, res) => {
             }
         }
         try {
-            const user = await User.findByIdAndUpdate(req.params.id, {
+            await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body
             })
             res.status(200).json(" account has been updated ")
